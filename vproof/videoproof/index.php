b0cc9b29de03ae56ba4389964ae01b511794b95b
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-07-12 03:49:40 +0000

    Deploy TypeNetwork/Roboto-Extremo to github.com/TypeNetwork/Roboto-Extremo.git:gh-pages

--- vproof/videoproof/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace TypeNetwork\VideoProof;
-require_once(__DIR__ . "/videoproof.inc");
-
-$videoproof = new VideoProof();
-
-print $videoproof->pageHead('Video Proof');
-print $videoproof->pageSections();
-print $videoproof->pageFoot();
